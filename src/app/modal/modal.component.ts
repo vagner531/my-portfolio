306abc37c110034cf0877b0d58abdf817362bba8
vagner santos <vagner131417@gmail.com>
2023-03-17 12:14:07 -0300

    feat: update modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,3 +1,4 @@
+import { MatDialogModule } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { GeneralService } from '../services/general.service';
 
@@ -8,10 +9,15 @@ import { GeneralService } from '../services/general.service';
 })
 export class ModalComponent implements OnInit {
   constructor(public generalService: GeneralService) { }
-
+  // MatDialogModule
   ngOnInit(): void {
+    this.generalService.showDialog=true;
     this.generalService.getEmailData().subscribe((data: any) => {
       this.generalService.emailData = data;
+      console.log(this.generalService.emailData);
+      console.log("2 - " + data);
+      
     });
   }
+   email = '@google';  
 }
